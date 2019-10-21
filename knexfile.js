2673734fb3f2b0a4b04aa3185784332a2a95b40b
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-10-21 14:21:49 -0400

    set up for heroku

--- knexfile.js
+++ knexfile.js
@@ -39,6 +39,19 @@ module.exports = {
      conn.run('PRAGMA foreign_keys = ON', done);    
     }
 
-  },  
+  },
+  //postgres
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,    
+    
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    
+  },    
 
 };
