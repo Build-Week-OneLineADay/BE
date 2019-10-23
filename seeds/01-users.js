6ff743150771c8dba37dd992d9481d4419969df7
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-10-23 14:19:48 -0400

    re-seeded with mockaroo data

--- seeds/01-users.js
+++ seeds/01-users.js
@@ -5,12 +5,25 @@ exports.seed = function(knex) {
   
       return knex('users').insert([
 
-        {first_name: 'john', last_name: 'doe', email: 'johndoe@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //1
-        {first_name: 'mary', last_name: 'jane', email: 'maryjane@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //2 
-        {first_name: 'pam', last_name: 'smith', email: 'pam@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //3
-        {first_name: 'eric', last_name: 'joe', email: 'eric@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //4
-        {first_name: 'enid', last_name: 'brown', email: 'enid@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //5
-        {first_name: 'jason', last_name: 'black', email: 'jason@yahoo.com', password: `${bcrypt.hashSync('test', 8)}`}, //6              
+        {
+          "id": 1,
+          "first_name": "John",
+          "last_name": "Doe",
+          "email": "johndoe@yahoo.com",
+          "password": `${bcrypt.hashSync('test', 8)}`
+        }, {
+          "id": 2,
+          "first_name": "Mary",
+          "last_name": "Jane",
+          "email": "maryjane@yahoo.com",
+          "password": `${bcrypt.hashSync('test', 8)}`
+        }, {
+          "id": 3,
+          "first_name": "Test",
+          "last_name": "User",
+          "email": "testuser@yahoo.com",
+          "password": `${bcrypt.hashSync('test', 8)}`
+        },             
         
       ]);
     
